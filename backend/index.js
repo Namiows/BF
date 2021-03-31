afd63325fe2c0ece893045f1b44b9ddc76971807
Kesley Lahoz <namielahoz@gmail.com>
2021-03-31 14:14:14 -0300

    Settingup backend with index.js and frontend with global context

---
+++ backend/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+
+const PORT = 3001;
+
+app.use(cors());
+
+app.use((req, _res, next) => {
+  console.log({
+    data: new Date(),
+    method: req.method,
+    router: req.originalUrl,
+  });
+  next();
+});
+
+app.use(bodyParser.json());
+
+app.use((err, _req, res) => {
+  console.error({ err });
+  res.status(500).json({ erro: 'erro interno' });
+});
+
+app.listen(PORT, () => console.log('running port', PORT));
